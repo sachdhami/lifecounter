58ef7a73605e6201e2d1526f093a49798caa08e8
Sachin Dhami <dhamis@uw.edu>
2024-04-17 09:42:06 -0700

    Initial Commit

---
+++ lifecounter2UITests/lifecounter2UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  lifecounter2UITestsLaunchTests.swift
+//  lifecounter2UITests
+//
+//  Created by Sachin Dhami on 4/17/24.
+//
+
+import XCTest
+
+final class lifecounter2UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
